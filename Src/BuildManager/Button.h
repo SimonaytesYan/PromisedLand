8c9bc597662c947e9cd75b49ab8e582c3a121e67
Simonaytes <yandexman228@gmail.com>
2024-04-02 21:25:28 +0300

    Implement Button

--- Src/BuildManager/Button.h
+++ Src/BuildManager/Button.h
@@ -1,11 +1,56 @@
 #pragma once
 
 #include "../GameObject.h"
+#include <functional>
+
+typedef void (*Function)(void* args);
+
+class Functor
+{
+public:
+    Functor(Function function, void* args) :
+    func(function),
+    args(args)
+    { }
+
+    void operator()()
+    {
+        func(args);
+    }
+
+protected:
+    Function func;
+    void*    args;
+};
 
 class Button : GameObject
 {
+
+    Button(int x, int y, size_t x_len, size_t y_len, const Functor& func) :
+    GameObject(x, y),
+    x_len     (x_len),
+    y_len     (y_len),
+    on_click  (func)
+    { }
     
     void draw(RenderTarget& render_target) override;
-    bool onClick(int x, int y) override;
-    void onTick() override;
+    bool onClick(int press_x, int press_y)
+    {
+        if (x <= press_x && press_x <= x + x_len &&
+            y <= press_y && press_y <= y + y_len)
+        {
+            on_click();
+            return true;
+        }
+        return false;
+    }
+
+    void onTick()
+    { }
+
+private:
+    size_t x_len;
+    size_t y_len;
+
+    Functor on_click;
 };
