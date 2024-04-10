db3cc07cf7ef34dbcc39baa7c59479929f04c3dd
Abubakirov Vladimir <abubakirov04@mail.ru>
2024-04-10 21:38:16 +0300

    Working on main

--- Src/main.cpp
+++ Src/main.cpp
@@ -3,67 +3,72 @@
 
 #include <SFML/Graphics.hpp>
 
-#include "GameLogic/Views/Tiles/CellView.hpp"
+#include "Graphics/Widget/BuildingPanel.hpp"
+#include "Graphics/CellView/CellViewCreator.hpp"
 #include "Constants.hpp"
 #include "Utils/RenderTarget.hpp"
-#include "GameLogic/Managers/ResourceManager.hpp"
-#include "GameLogic/Views/TextView/ResourceBar.hpp"
-#include "GameLogic/Views/Window.hpp"
+#include "Graphics/Widget/Window.hpp"
 
-// void runGameCycle(sf::RenderWindow& window, RenderTarget& rt) 
-// {
-// 	// just testing
-// 	ResourceManager resource_manager;
-// 	Window          game_window({0, 0}, resource_manager);
+void generateField(Window& window, CellInterlayer& cell_int, const sf::Vector2u window_size)
+{
+	// leave space for controls below
+	const int x_cell_cnt = (window_size.x - kControlPanelW) / kFieldSize;
+	const int y_cell_cnt = (window_size.y - kControlPanelH) / kFieldSize;
 
-// 	CellView* test_cell1 = new CellView({0, 0}, FieldType::House, resource_manager);
-// 	CellView* test_cell2 = new CellView({0, 0}, FieldType::House, resource_manager);
-// 	CellView* test_cell3 = new CellView({0, 0}, FieldType::House, resource_manager);
+	for (int i = 0; i <= x_cell_cnt; ++i) 
+	{
+		for (int j = 0; j <= y_cell_cnt; ++j) 
+		{
+			const int cell_x    = i * kFieldSize;
+			const int cell_y    = j * kFieldSize;
+			
+			// window.
+		}
+	}
+}
 
-// 	game_window.addChild(test_cell1);
-// 	game_window.addChild(test_cell2);
-// 	game_window.addChild(test_cell3);
+void runGameCycle(sf::RenderWindow& window, RenderTarget& rt) 
+{
+	Window game_window({0, 0});
+	BuildingPanel
 
-// 	game_window.addChild(new ResourceBar(1920, 50, resource_manager));
+    auto timer_start = std::chrono::system_clock::now(); 
+    while (window.isOpen())
+	{
+		auto timer_end = std::chrono::system_clock::now();
+        auto passed   = std::chrono::duration_cast<std::chrono::milliseconds>(timer_end - timer_start);
+		if (passed.count() >= kMSInClock)
+		{
+			// Event tick_event(EventType::TICK);
+			// game_window.push(&tick_event);
 
-//     auto timer_start = std::chrono::system_clock::now(); 
-//     while (window.isOpen())
-// 	{
-// 		auto timer_end = std::chrono::system_clock::now();
-//         auto passed   = std::chrono::duration_cast<std::chrono::milliseconds>(timer_end - timer_start);
-// 		if (passed.count() >= kMSInClock)
-// 		{
-// 			Event tick_event(EventType::TICK);
-// 			game_window.push(&tick_event);
+			timer_start = timer_end;
+		}
 
-// 			timer_start = timer_end;
-// 		}
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+				{
+					window.close();
+				}
 
-// 		sf::Event event;
-// 		while (window.pollEvent(event))
-// 		{
-// 			switch (event.type)
-// 			{
-// 				case sf::Event::Closed:
-// 				{
-// 					window.close();
-// 				}
+				case sf::Event::MouseButtonPressed:
+				{
 
-// 				case sf::Event::MouseButtonPressed:
-// 				{
+				}
+			}
+		}
 
-// 				}
-// 			}
-// 		}
-// 		rt.clear();
-// 		window.clear();
+		rt.clear();
+		window.clear();
 
-// 		game_window.draw(rt);
-
-// 		rt.display(window);
-// 		window.display();
-//     }
-// }
+		rt.display(window);
+		window.display();
+    }
+}
 
 int main()
 {
@@ -72,5 +77,8 @@ int main()
     sf::RenderWindow window(sf::VideoMode(), kWindowHeader, sf::Style::Fullscreen);
 	RenderTarget main_rt(window.getSize());
 
-	// runGameCycle(window, main_rt);
+	ResourceBarInterlayer res_bar_interlayer;
+	ResourceManager res_manager;
+
+	runGameCycle(window, main_rt);
 }
