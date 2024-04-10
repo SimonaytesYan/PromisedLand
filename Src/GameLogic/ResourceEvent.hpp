877bca722749aa52d69bba10c3eb8146c1a31619
Simonaytes <yandexman228@gmail.com>
2024-04-10 05:28:38 +0300

    Move ResourceEvents

---
+++ Src/GameLogic/ResourceEvent.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../Eventable.hpp"
+#include "../Logics/Resources.hpp"
+
+struct ResourceEvent : public Event
+{
+    explicit ResourceEvent(const EventType _event_type, const Resources _resources = kStartResources)
+      : Event         (_event_type),
+        add_resources (_resources)
+    {}
+
+    Resources add_resources;
+};
