94549a45e2230be51b23c9688cba9acc294b4cad
Simonaytes <yandexman228@gmail.com>
2024-04-11 00:26:22 +0300

    Add file Events.hpp

--- Src/Events/Eventable.hpp
+++ Src/Events/Eventable.hpp
@@ -1,56 +1,6 @@
 #pragma once
 
-#include "../Utils/GraphicPrimitives.hpp"
-
-enum class EventType 
-{
-    // PHYSICAL EVENTS
-    TICK,
-    MOUSE_CLICK,
-
-    // VIRTUAL EVENTS
-    SELECT_BUILDING_EVENT,
-    RESOURCES_CHANGED_EVENT,
-};
-
-struct Event 
-{
-    explicit Event(const EventType _event_type)
-      : event_type (_event_type)
-    {}
-
-    EventType event_type;
-};
-
-struct MouseEvent : public Event
-{
-    MouseEvent(Point pos)
-    : Event(EventType::MOUSE_CLICK),
-      pos (pos)
-    { }
-
-    Point pos;
-};
-
-struct SelectBuildingEvent : public Event
-{
-    SelectBuildingEvent(FieldType type)
-    : Event(EventType::SELECT_BUILDING_EVENT),
-      type (type)
-    { }
-
-    FieldType type;
-};
-
-struct ResourceEvent : public Event
-{
-  explicit ResourceEvent(const Resources _resources)
-    : Event     (EventType::RESOURCES_CHANGED_EVENT),
-      resources (_resources)
-    {}
-
-  Resources resources;
-};
+#include "Events.hpp"
 
 struct Eventable 
 {
---
+++ Src/Events/Events.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "../Utils/GraphicPrimitives.hpp"
+#include "../GameLogic/Resources.hpp"
+
+enum class EventType 
+{
+    // PHYSICAL EVENTS
+    TICK,
+    MOUSE_CLICK,
+
+    // VIRTUAL EVENTS
+    SELECT_BUILDING_EVENT,
+    RESOURCES_CHANGED_EVENT,
+};
+
+struct Event 
+{
+    explicit Event(const EventType _event_type)
+      : event_type (_event_type)
+    {}
+
+    EventType event_type;
+};
+
+struct MouseEvent : public Event
+{
+    MouseEvent(Point pos)
+    : Event(EventType::MOUSE_CLICK),
+      pos (pos)
+    { }
+
+    Point pos;
+};
+
+struct SelectBuildingEvent : public Event
+{
+    SelectBuildingEvent(FieldType type)
+    : Event(EventType::SELECT_BUILDING_EVENT),
+      type (type)
+    { }
+
+    FieldType type;
+};
+
+struct ResourceEvent : public Event
+{
+  explicit ResourceEvent(const Resources _resources)
+    : Event     (EventType::RESOURCES_CHANGED_EVENT),
+      resources (_resources)
+    {}
+
+  Resources resources;
+};
