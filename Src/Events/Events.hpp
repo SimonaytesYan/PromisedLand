9cc88fc9af8c56c54ae5f3a3cc836ac30d88cdbb
Simonaytes <yandexman228@gmail.com>
2024-04-17 17:00:07 +0300

    Update events

--- Src/Events/Events.hpp
+++ Src/Events/Events.hpp
@@ -3,6 +3,8 @@
 #include "../Utils/GraphicPrimitives.hpp"
 #include "../GameLogic/Resources.hpp"
 
+class CellView;
+
 enum class EventType 
 {
     // PHYSICAL EVENTS
@@ -12,6 +14,8 @@ enum class EventType
     // VIRTUAL EVENTS
     SELECT_BUILDING_EVENT,
     RESOURCES_CHANGED_EVENT,
+    BUILD_CELL_EVENT,
+    DESTROY_CELL_EVENT,
 };
 
 struct Event 
@@ -51,4 +55,24 @@ struct ResourceEvent : public Event
     {}
 
   Resources resources;
+};
+
+struct BuildCellEvent : public Event
+{
+  explicit BuildCellEvent(CellView* _cell_view)
+    : Event     (EventType::BUILD_CELL_EVENT),
+      cell_view (_cell_view)
+    {}
+
+  CellView* cell_view;
+};
+
+struct DestroyCellEvent : public Event
+{
+  explicit DestroyCellEvent(CellView* _cell_view)
+    : Event     (EventType::DESTROY_CELL_EVENT),
+      cell_view (_cell_view)
+    {}
+
+  CellView* cell_view;
 };
