e971554ca7d32a7198e059953dd81b4d4aa5c57b
Simonaytes <yandexman228@gmail.com>
2024-04-18 22:26:41 +0300

    Use Cell::createInstance in CellManager::createCell

--- Src/Managers/CellManager.cpp
+++ Src/Managers/CellManager.cpp
@@ -3,41 +3,10 @@
 
 void CellManager::createCell(const Point position)
 {
-    Cell* new_cell = nullptr;
+    Cell* new_cell = Cell::createInstance(cell_type);
 
-    switch (cell_type)
-    {
-    case FieldType::Forest:
-        new_cell = new Grass();
-        break;
-    case FieldType::Grass:
-        new_cell = new Grass();
-        break;
-    case FieldType::Water:
-        new_cell = new Water();
-        break;
-    case FieldType::House:
-      {
-        new_cell = new House();
+    if (cell_type == FieldType::House)
         cell_interlayer->pushToLogic(new NewCitizensEvent(static_cast<House*>(new_cell)->getAppearIncome().population));
-
-        break;
-      }
-    case FieldType::Sawmill:
-        new_cell = new Sawmill();
-        break;
-    case FieldType::Well:
-        new_cell = new Well();
-        break;
-    case FieldType::Windmill:
-        new_cell = new Windmill();
-        break;
-    case FieldType::FieldNumber:
-    case FieldType::Error:
-        break;
-    default:
-        break;
-    }
 
     createCell(new_cell);
     res_manager->onBuild(new_cell);
