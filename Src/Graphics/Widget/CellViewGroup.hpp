99b2f3b875e1bb5a6765a00fad37b75c853cfb5d
Simonaytes <yandexman228@gmail.com>
2024-04-18 22:11:49 +0300

    Remove useless function

--- Src/Graphics/Widget/CellViewGroup.hpp
+++ Src/Graphics/Widget/CellViewGroup.hpp
@@ -29,7 +29,6 @@ protected:
     // Put here pointers, created by new.
     // Here you transfer ownership of cell to CellViewGroup
     void addCell(CellView* cell_view);
-    void addCell(const FieldType field_type, const Point pos);
     void addCell(const Texture texture, const Point pos);
 
     void deleteDeadCells();
