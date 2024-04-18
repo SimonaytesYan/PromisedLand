2db8a3e03bceadc026fcbaeb03bee4ff020d9af9
Simonaytes <yandexman228@gmail.com>
2024-04-19 00:11:16 +0300

    Remove debug outputs

--- Src/Managers/CellManager.cpp
+++ Src/Managers/CellManager.cpp
@@ -18,15 +18,10 @@ void CellManager::tryBuildCell(const size_t index, const Point position)
     
     const unsigned char on_which_build = static_cast<unsigned char>(cells[index]->getFieldType());
     const Bitmask       build_mask     = static_cast<Building*>    (new_building)->getBuildMask();
-
-    fprintf(stderr, "build_mask        = %b\n", build_mask);
-    fprintf(stderr, "new_building type = %d\n", new_building->getFieldType());
-    fprintf(stderr, "on_which_build    = %d\n", on_which_build);
     delete new_building;
     
     if (getBit(build_mask, on_which_build))
     {
-        fprintf(stderr, "Rebuild\n");
         deleteCell(index);
         createCell(position);
 
