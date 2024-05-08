e48f0e4b084d8822f7ac91fb7e0367f0cffcac6d
Simonaytes <yandexman228@gmail.com>
2024-05-08 18:28:21 +0300

    Fix UI

--- Src/Menu/Menu.cpp
+++ Src/Menu/Menu.cpp
@@ -38,7 +38,7 @@ void CreateGameWindowAndRunGameCycle(MenuButtonArgs args)
 	game_window.addChild(build_panel);
 
 	BasicFunctor* save_func = new Functor<CellInterlayer&>(SaveMap, cell_interlayer);
-	Button* save_button = new Button({args.window.getSize().x - 150, 50}, 100, 100, 
+	Button* save_button = new Button({args.window.getSize().x - 112, 12}, 100, 100, 
 									  save_func, "Assets/UI/SaveIcon.png");
 	game_window.addChild(save_button);
 
