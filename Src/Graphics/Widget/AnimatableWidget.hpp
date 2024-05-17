a0a5444e736626d2d154c732b69adb38edd2af22
Simonaytes <yandexman228@gmail.com>
2024-05-18 01:44:51 +0300

    Add frame_duration in AnimatedWidget

--- Src/Graphics/Widget/AnimatableWidget.hpp
+++ Src/Graphics/Widget/AnimatableWidget.hpp
@@ -1,20 +1,23 @@
 #pragma once
 
+#include <chrono>
+
 #include "Widget.hpp"
 #include "../../../StlVector/Src/Vector.hpp"
 
 class AnimatedWidget : public Widget
 {
+    typedef std::chrono::_V2::system_clock::time_point time_point;
 public:
     AnimatedWidget(const Point pos, Vector<Texture>&& images) : 
     Widget        (pos),
     images        (images),
-    image_counter (0)
+    prev_time     (std::chrono::high_resolution_clock::now())
     { }
 
     AnimatedWidget(const Point pos, const Vector<const char*>& files) : 
     Widget        (pos),
-    image_counter (0)
+    prev_time     (std::chrono::high_resolution_clock::now())
     {
         const size_t files_number = files.Size();
         for (size_t i = 0; i < files_number; i++)
@@ -25,16 +28,28 @@ public:
     {
         if (image_counter < images.Size())
             rt.drawTexture(pos, images[image_counter]);
+        time_point now = std::chrono::high_resolution_clock::now();
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - prev_time).count() > 100)
+        {
+            prev_time = now;
 
-        image_counter++;
-        if (image_counter >= images.Size())
-            image_counter = 0;
+            image_counter++;
+            if (image_counter >= images.Size())
+                image_counter = 0;
+        }
+    }
+
+    void setFrameDuration(const size_t new_frame_duration)
+    {
+        frame_duration = new_frame_duration;
     }
 
     void push(EventPtr event) override
     { }
 
 private:
+    size_t          image_counter  = 0;
+    size_t          frame_duration = 100;
     Vector<Texture> images;
-    size_t          image_counter;
+    time_point      prev_time;
 };
